4c0654ef503fdb0e56438b287282efd5921207ba
erwinwkjk <ewijanarko20@gmail.com>
2024-08-30 11:04:35 +0700

    Menyelesaikan CRUD Article Management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArtikelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('artikels', ArtikelController::class);
+Route::get('artikels/{id}', [ArtikelController::class, 'show'])->name('artikels.show');
+
+
